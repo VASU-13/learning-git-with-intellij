2ba6f908ab9bdce36c4798be0c871388745953eb
Codeamon <codegeek404@gmail.com>
2022-09-10 23:33:42 +0530

    added feature2

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -3,5 +3,11 @@ public class DevClass {
     public static void main(String[] args) {
 
         System.out.println("Inside Dev Class");
+        
+        // Changes from Another Account
+        
+        System.out.println("Added feature2");
+        
+        
     }
 }
